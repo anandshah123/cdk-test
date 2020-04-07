4028d943dbfa35c36a49bc0427d14333d7de82b0
Anand Shah <annshah@amazon.lu>
2020-04-07 17:29:52 +0200

    Fixed issue

--- lib/sample-app-stack.ts
+++ lib/sample-app-stack.ts
@@ -5,11 +5,11 @@ export class SampleAppStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const s3Bucket1 = new Bucket(this, "testBucket", {
+        const s3Bucket1 = new Bucket(this, "b1", {
             blockPublicAccess: BlockPublicAccess.BLOCK_ALL
         });
 
-        const s3Bucket2 = new Bucket(this, "testBucket", {
+        const s3Bucket2 = new Bucket(this, "b2", {
             blockPublicAccess: BlockPublicAccess.BLOCK_ALL
         });
 
