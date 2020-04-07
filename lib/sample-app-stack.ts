d6ac14fa4b000df285294bc2ee06d40f9f5cbc12
Anand Shah <annshah@amazon.lu>
2020-04-07 16:08:55 +0200

    Added another bucket

--- lib/sample-app-stack.ts
+++ lib/sample-app-stack.ts
@@ -1,14 +1,17 @@
 import * as cdk from '@aws-cdk/core';
 import {BlockPublicAccess, Bucket} from "@aws-cdk/aws-s3";
-import {Aws} from "@aws-cdk/core";
 
 export class SampleAppStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const s3Bucket = new Bucket(this, "testBucket", {
-            // bucketName: 'test-bucket-' + Aws.ACCOUNT_ID,
+        const s3Bucket1 = new Bucket(this, "testBucket", {
             blockPublicAccess: BlockPublicAccess.BLOCK_ALL
         });
+
+        const s3Bucket2 = new Bucket(this, "testBucket", {
+            blockPublicAccess: BlockPublicAccess.BLOCK_ALL
+        });
+
     }
 }
